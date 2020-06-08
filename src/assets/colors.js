f31c99d6a248e076d867a844d1a5d3c51c2a1c0f
Marco Aurélio <marcooli.pereira@gmail.com>
2020-06-08 00:50:51 -0300

    Adicionando arquivos de imagens e cores

---
+++ src/assets/colors.js
@@ -0,0 +1,8 @@
+export default {
+   blue: '#0C354B',
+   orange: '#FE924D',
+   red: '#FA575F',
+   white: '#EEEFF1',
+   text: '#BEBEBE',
+   textHover: '#F0FFFF'
+}
---
+++ src/assets/images/index.js
@@ -0,0 +1,7 @@
+import Logo from '../images/logo.png';
+import Profile from '../images/eu.png';
+
+export default {
+   Logo,
+   Profile
+};
