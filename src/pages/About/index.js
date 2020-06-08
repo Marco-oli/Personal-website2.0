93294cd92266c5c7470faad6d7d1d672629424ba
Marco Aurélio <marcooli.pereira@gmail.com>
2020-06-08 16:31:24 -0300

    Adicionando page About

---
+++ src/pages/About/index.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from 'react';
+
+import CardTecnology from '../../components/CardTecnology';
+import IconAbout from '../../components/IconAbout';
+
+import { IoMdMusicalNotes } from 'react-icons/io';
+
+import api from '../../services/api';
+import images from '../../assets/images';
+import colors from '../../assets/colors';
+
+import * as S from './styles';
+
+const About = () => {
+
+   const [dados, setDados] = useState([]);
+   const [tecnology, setTecnology] = useState([]);
+
+   useEffect(() => {
+      api.get('/pessoal').then(response => {
+         setDados(response.data.sobre)
+      })
+   }, [])
+
+   useEffect(() => {
+      api.get('/pessoal').then(response => {
+         setTecnology(response.data.tecnology)
+      })
+   }, [])
+
+   return (
+      <S.Container>
+         <div>
+            <h1>Sobre</h1>
+         </div>
+         
+         <S.SoftSkills>
+
+            {(dados) ? dados.map(item => (
+               <S.Skills key={item.id}>
+                  <div>
+                     <IconAbout icon={item.icon}/>
+                  </div>
+                  <h4>{item.title}</h4>
+                  <p>{item.subtitle}</p>
+               </S.Skills>
+            )) : console.log('error')}
+
+         </S.SoftSkills>
+
+         <S.Profile>
+            <S.BoxProfile>
+               <S.BoxProfileImage>
+                  <img src={images.Profile} alt=""/>
+               </S.BoxProfileImage>
+               <S.BoxProfileTitle>
+                  <h2>Esse cara sou eu</h2>
+                  <IoMdMusicalNotes size={25} color={colors.red}/>
+               </S.BoxProfileTitle>
+               <S.BoxProfileDescription>
+                  <p>Sou desenvolvedor front-end do <span>Rio de Janeiro</span> - RJ, Brasil.
+                     Criei uma grande paixão por interfaces web e mobile, animações e <span>criação de experiências intuitivas</span> e dinâmicas para o usuário.
+                     Vamos criar algo juntos!</p>
+               </S.BoxProfileDescription>
+            </S.BoxProfile>
+
+            <S.Tecnology>
+            {(tecnology) ? tecnology.map(item => (
+              
+                  <S.BoxTecnology key={item.id}>
+                     <CardTecnology icon={item.icon} />
+                     <p>{item.title}</p>
+                  </S.BoxTecnology>
+               
+               
+            )) : null}
+
+            </S.Tecnology>
+            
+         </S.Profile>
+      </S.Container>
+   )
+}
+
+export default About;
---
+++ src/pages/About/styles.js
@@ -0,0 +1,150 @@
+import styled from 'styled-components';
+import colors from '../../assets/colors';
+
+export const Container = styled.div`
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   width: 100%;
+   min-height: 100vh;
+   color: ${colors.textAbout};
+   background-color: #EFF9FB;
+   
+   h1 {
+      color: ${colors.titleAbout};
+      margin-top: 50px;
+      font-size: 40px;
+      letter-spacing: 0px;
+      text-transform: uppercase;
+
+      ::after {
+         content: '';
+         width: 50px;
+         height: 1px;
+         background-color: currentColor;
+         display: block;
+         align-items: center;
+         
+      }
+   }
+`;
+
+export const SoftSkills = styled.header`
+   display: flex;
+   justify-content: space-evenly;
+   max-width: 80%;
+   margin-top: 80px;
+`;
+
+export const Skills = styled.div`
+   display: flex;
+   align-items: center;
+   flex: 0.20;
+   flex-direction: column;
+
+   div {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: ${colors.blueStrong};
+      border-radius: 30px;
+      width: 100px;
+      height: 100px;
+      
+   }
+
+   h4 {
+      font-size: 28px;
+      margin-top: 5px;
+   }
+
+   p {
+      line-height: 20px;
+   }
+`;
+
+export const Profile =styled.main`
+   display: flex;
+   flex-direction: row;
+   justify-content: space-around;
+   max-width: 70%;
+   flex: 1;
+   margin-top: 70px;
+   margin-bottom: 30px;
+`;
+
+export const BoxProfile =styled.main`
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   flex: 0.7;
+`;
+
+export const BoxProfileImage = styled.div`
+  img {
+      border-radius: 30px;
+      width: 250px;
+      height: 210px;
+      box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.1);
+  }
+`;
+
+export const BoxProfileTitle = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  h2 {
+      font-size: 28px;
+      letter-spacing: 0px;
+      margin-right: 5px;
+  }
+`;
+
+export const BoxProfileDescription = styled.div`
+   display: flex;
+   justify-content: center;
+
+   p {
+     max-width: 80%;
+   }
+
+   span {
+      color: ${colors.blueStrong};
+      font-weight: 500;
+   }
+`;
+
+export const Tecnology = styled.div`
+   display: grid;
+   grid-template-columns: 1fr 1fr;
+   gap: 20px;
+   width: 50%;
+`;
+
+export const BoxTecnology = styled.div`
+   display: flex;
+   flex-direction: row;
+   align-items: center;
+   justify-content: space-evenly;
+   max-height: 110px;
+   max-width: 110px;
+   padding: 5px;
+   background-color: #fff;
+   border-radius: 50%;
+   box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.3);
+
+   p {
+      font-size: 18px;
+      font-weight: bold;
+   }
+
+`;
+
+
+
+  
+
+   
+
+
